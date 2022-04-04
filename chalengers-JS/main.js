58dcd41d670c1f217955f6e25704dc491a7f8d80
Marconio dos Santos <marconio.dev@gmail.com>
2022-04-03 23:58:13 -0300

    first kata

---
+++ chalengers-JS/main.js
@@ -0,0 +1,13 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+----------------------My--------------------------------------
+const booleanToString = b => b ? 'true' : 'false'
+
+
+--------------------codewars----------------------------------
+function booleanToString(b){
+    return b.toString();
+  }
+*/
