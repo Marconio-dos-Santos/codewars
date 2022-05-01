b067a694ebd12a402ac3946427f32e73fc633ebc
Marconio dos Santos <marconio.dev@gmail.com>
2022-05-01 14:07:50 -0300

    8kyu oposite number

---
+++ 8-kyu/EvenOrOdd/evenOrOdd.js
@@ -0,0 +1,9 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(int){
+    if(int % 2 === 0){
+        return 'Even'
+    }else{
+        return 'Odd'
+    }
+}
---
+++ 8-kyu/oppositeNmber/opositeNumber.js
@@ -0,0 +1,6 @@
+// Very simple, given an integer or a floating-point number.
+
+    function opposite(number) {
+        //oposite Number
+        return(-number)
+        }
