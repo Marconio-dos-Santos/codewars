cc5411189f6e2af0c5814845578971877f08968c
Marconio dos Santos <marconio.dev@gmail.com>
2022-05-13 16:10:04 -0300

    atualizaão de pastas + novo desafio 8Kyu

---
+++ 8-kyu/JavaScript/solutionsJS009.js
@@ -0,0 +1,21 @@
+
+// Notes
+
+  //make it negative 1 to -1, -5 to -5, 0 to 0, 0,12 to -0,12
+  // P: 
+  // number, negative or not, can be float
+  // check if is negative number 
+  // if not make it negative negative
+
+  // R:
+  // console/return
+  // E:
+  function makeNegative(num) {
+    return -num
+  
+}
+
+makeNegative(1)     // return -1
+makeNegative(-5)    // return -5
+makeNegative(0)     // return 0
+makeNegative(0.12)  // return -0,12
