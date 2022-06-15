8b96dd03b261b2cece355804bcc736593e285c26
Marconio dos Santos <marconio.dev@gmail.com>
2022-06-15 10:04:53 -0300

    metedo map para dobrar cada valor em umarray attualizado

---
+++ 8-kyu/LostWithoutaMap.js
@@ -0,0 +1,9 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+function maps(x){
+    let dble = x.map(n => n * 2);
+      return dble
+  }
