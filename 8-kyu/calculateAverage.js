969eef5c186a00617bd6cc53ee4a7b1174579cfb
Marconio dos Santos <marconio.dev@gmail.com>
2022-06-29 10:27:27 -0300

    8kyu array average

---
+++ 8-kyu/calculateAverage.js
@@ -0,0 +1,12 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+function find_average(array) {
+    let average = 0
+    if(array.length == 0) return 0
+    for(let i = 0 ; i < array.length; i++){
+      average += array[i]
+    }
+    console.log(average / array.length)
+    return average / array.length;
+  }
