de65815cfc1e917536a46ad5dcb101635a3611a2
Marconio dos Santos <marconio.dev@gmail.com>
2022-07-06 10:20:50 -0300

    8kyu calculate IBM with condicionals

---
+++ 8-kyu/calculateBMI.js
@@ -0,0 +1,22 @@
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+function bmi(weight, height) {
+    let bmi = weight/height**2
+    console.log(bmi)
+    if(bmi <= 18.5){
+      return "Underweight"
+    }if(bmi <= 25.0){
+      return "Normal"
+    }if(bmi <= 30.0){
+      return "Overweight"
+    }else{
+      return "Obese"
+    }
+  }
