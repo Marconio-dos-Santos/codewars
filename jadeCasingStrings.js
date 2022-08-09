9babd0a379375281cff4ea45e8273300b347e5f1
Marconio dos Santos <marconio.dev@gmail.com>
2022-08-09 00:18:30 -0300

    wip

--- jadeCasingStrings.js
+++ jadeCasingStrings.js
@@ -2,7 +2,6 @@
 
 // Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
 
-// Example:
 
 // Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 // Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
