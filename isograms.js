e54953f04916588b1ec867f648a261f92dbd124c
Marconio dos Santos <marconio.dev@gmail.com>
2022-08-12 03:56:26 -0300

    2 for loop para comparar todos os elementos do array

---
+++ isograms.js
@@ -0,0 +1,25 @@
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+// Example: (Input --> Output)
+
+// "Dermatoglyphics" --> true
+// "aba" --> false
+// "moOse" --> false (ignore letter case)
+
+function isIsogram(str) {
+    str = str.toLowerCase();
+  
+    for (first = 0; first < str.length - 1; first++) {
+      for (second = first + 1; second < str.length; second++) {
+        if (str[first] === str[second]) {
+          return false;
+        }
+      }
+    }
+  
+    return true;
+  }
+
+  //ou 
+
+  
