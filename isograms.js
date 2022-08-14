3a3e6cacb85accdad6476177311fa8e93ec06c61
Marconio dos Santos <marconio.dev@gmail.com>
2022-08-13 23:48:24 -0300

    ordenacao modo crescente

--- isograms.js
+++ isograms.js
@@ -21,5 +21,19 @@ function isIsogram(str) {
   }
 
   //ou 
+  function isIsogram(str){
+
+    str = str.toLowerCase ();
+    
+     for (let i=0; i < str.length ; i++) {
+     
+           for (let j= i + 1; j < str.length; j++) {
+           
+           if (str[i] === str[j])
+           return false
+           }
+     }
+     return true;
+        }
 
   
